bf50a82f23c24e4ab1d78a58c9ffe8f9fb71aa8c
Mikhail Polykovskij <glader@yandex-team.ru>
2012-03-21 13:49:07 +0600

    Server initialization

--- fabfile.py
+++ fabfile.py
@@ -13,22 +13,35 @@ env.manage_dir = env.directory + '/src'
 env.deploy_user = 'www'
 env.user = 'www'
 env.activate = 'source %s/ENV/bin/activate' % env.directory
+env.www_ssh_key = 'ssh-dss AAAAB3NzaC1kc3MAAACAbN+8KDO1jkRluNqiqO2KjkaSn4Qs66zBcV+JaUFrnoVt5tBaEMGW56ihtd1zmPqSufpDKTMXKneZWLAx8evFobvU5S32OKtFpR6oylZwIWg0SQNtjBE7lFHC5VnN4BtjpLp6DBzUOt6mTXYyCjaYhorMWmyw5641KXOsW0V7et0AAAAVALlYgGve+sIVrw7MTQFD4Hvb1utVAAAAgAGktSDpYw1sEC9tA593z3Ymk9r4J939DsKiL3d+RK/RXfY9KgoFtMHmCzL8goYpyWdaE2XQzCrIfp3EFW41NUWUfxsaDzXSEg4Q/CYAfJm7nNDpwv1eAq3c0Mw7RMGEw3pxsAnQrq0snHI7cVhdZ12Z6wO147+ybAbOXW7XF04sAAAAgGzFeuezmdfyS0N4VE42/kgC4SusMTxYOj5nrb8VRvzQ08Msa5FChXIWv0Fj5hMpOVX/gc4uEkbt7knpjqouo+K+8jadQ4I+sRidqG13U6b2UGJy844THSqL3HIhuPmhvWPOFjJbsNFxcoakSqLxn3ewkDzco7CH/aYo9u9VrLwk dsa-key-20080514'
 
 def virtualenv(command):
     with cd(env.directory):
         run(env.activate + ' && ' + command)
 
-
-@hosts('ec2-23-20-142-92.compute-1.amazonaws.com')
 def init():
-    packages = ('lighttpd', 'mysql-server', 'mysql-client', 'build-dep', 'python-mysqldb',
-                'python-dev', 'runit', 'rrdtool', 'sendmail')
-    for package in packages:
-        sudo('apt-get install %s' % package)
+    env.deploy_user = 'ubuntu'
+    env.user = 'ubuntu'
+
+    sudo('apt-get update')
+    sudo('apt-get install -y mc lighttpd mysql-client git-core python-setuptools python-dev runit rrdtool sendmail memcached libjpeg62-dev')
+    sudo('apt-get build-dep -y python-mysqldb')
+
+    if not exists('/home/www'):
+        sudo('yes | adduser --disabled-password www')
+        sudo('mkdir /home/www/.ssh')
+        sudo('echo "%s" >> /home/www/.ssh/authorized_keys' % env.www_ssh_key)
+        sudo('chown -R www:www /home/www')
+
+    if not exists('/var/log/projects/gladerru'):
+        sudo('mkdir -p /var/log/projects/gladerru')
+        sudo('chown -R www:www /var/log/projects/gladerru')
 
 
-@hosts('ec2-23-20-142-92.compute-1.amazonaws.com')
 def production():
+    env.deploy_user = 'www'
+    env.user = 'www'
+
     upload()
     static()
     environment()
@@ -42,6 +55,7 @@ def production():
 
 
 def upload():
+    run('mkdir -p /home/www/projects/gladerru')
     local('git archive -o archive.tar.gz HEAD')
     put('archive.tar.gz', env.directory + '/archive.tar.gz')
     with cd(env.directory):
@@ -51,7 +65,7 @@ def upload():
 
 
 def static():
-    with cd(os.path.join(env.directory, 'src/media/design/3/css')):
+    with cd(env.directory + '/src/media/design/3/css'):
         run('python merge.py')
 
 
