8887638f9139dda14c5d20b2e6ce8be1d3e68fba
Mikhail Polykovskij <glader@yandex-team.ru>
2012-03-22 11:38:38 +0600

    Deployment

--- fabfile.py
+++ fabfile.py
@@ -3,17 +3,22 @@
 import os
 
 from fabric.api import *
-from fabric.contrib.files import exists, sed, upload_template
+from fabric.operations import prompt
+from fabric.contrib.files import exists, append, sed, upload_template
 
 from fab_settings import *
 
-env.hosts = ['ec2-23-20-142-92.compute-1.amazonaws.com']
+env.ami = 'ami-8f03ede6'
 env.directory = '/home/www/projects/gladerru'
 env.manage_dir = env.directory + '/src'
 env.deploy_user = 'www'
 env.user = 'www'
 env.activate = 'source %s/ENV/bin/activate' % env.directory
 env.www_ssh_key = 'ssh-dss AAAAB3NzaC1kc3MAAACAbN+8KDO1jkRluNqiqO2KjkaSn4Qs66zBcV+JaUFrnoVt5tBaEMGW56ihtd1zmPqSufpDKTMXKneZWLAx8evFobvU5S32OKtFpR6oylZwIWg0SQNtjBE7lFHC5VnN4BtjpLp6DBzUOt6mTXYyCjaYhorMWmyw5641KXOsW0V7et0AAAAVALlYgGve+sIVrw7MTQFD4Hvb1utVAAAAgAGktSDpYw1sEC9tA593z3Ymk9r4J939DsKiL3d+RK/RXfY9KgoFtMHmCzL8goYpyWdaE2XQzCrIfp3EFW41NUWUfxsaDzXSEg4Q/CYAfJm7nNDpwv1eAq3c0Mw7RMGEw3pxsAnQrq0snHI7cVhdZ12Z6wO147+ybAbOXW7XF04sAAAAgGzFeuezmdfyS0N4VE42/kgC4SusMTxYOj5nrb8VRvzQ08Msa5FChXIWv0Fj5hMpOVX/gc4uEkbt7knpjqouo+K+8jadQ4I+sRidqG13U6b2UGJy844THSqL3HIhuPmhvWPOFjJbsNFxcoakSqLxn3ewkDzco7CH/aYo9u9VrLwk dsa-key-20080514'
+
+if not env.hosts:
+    env.hosts = ['ec2-23-20-142-92.compute-1.amazonaws.com']
+
 
 def virtualenv(command):
     with cd(env.directory):
@@ -33,9 +38,15 @@ def init():
         sudo('echo "%s" >> /home/www/.ssh/authorized_keys' % env.www_ssh_key)
         sudo('chown -R www:www /home/www')
 
+    append('/etc/sudoers', 'www  ALL=(ALL) NOPASSWD:/usr/bin/sv', use_sudo=True)
+
     if not exists('/var/log/projects/gladerru'):
         sudo('mkdir -p /var/log/projects/gladerru')
-        sudo('chown -R www:www /var/log/projects/gladerru')
+        sudo('chmod 777 /var/log/projects/gladerru')
+
+    if not exists('/var/cache/gladerru/thumbnails'):
+        sudo('mkdir -p /var/cache/gladerru/thumbnails')
+        sudo('chown -R www:www /var/cache/gladerru')
 
 
 def production():
@@ -90,6 +101,11 @@ def lighttpd():
     sudo('cp %(directory)s/tools/lighttpd/90-gladerru.conf /etc/lighttpd/conf-available/90-gladerru.conf' % env, shell=False)
     if not exists('/etc/lighttpd/conf-enabled/90-gladerru.conf'):
         sudo('ln -s /etc/lighttpd/conf-available/90-gladerru.conf /etc/lighttpd/conf-enabled/90-gladerru.conf', shell=False)
+
+    if not exists('/etc/lighttpd/conf-available/10-modules.conf'):
+        sudo('cp %(directory)s/tools/lighttpd/10-modules.conf /etc/lighttpd/conf-available/10-modules.conf' % env, shell=False)
+        sudo('ln -s /etc/lighttpd/conf-available/10-modules.conf /etc/lighttpd/conf-enabled/10-modules.conf', shell=False)
+
 #    sudo('/etc/init.d/lighttpd reload', shell=False)
 
 
