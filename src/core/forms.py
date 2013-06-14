8574ea5a6afe5e3d59cfd9747d605016d7e12144
Mikhail Polykovskij <glader@yandex-team.ru>
2013-06-14 11:42:42 +0600

    BeautifulSoup 4

--- src/core/forms.py
+++ src/core/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
-from BeautifulSoup import BeautifulSoup, Comment as HtmlComment
+from bs4 import BeautifulSoup, element
 import urllib
 import random
 from datetime import datetime
@@ -23,19 +23,23 @@ def sanitizeHTML(value, mode='none'):
         valid_tags = 'p i strong b u a h1 h2 h3 h4 pre br div span img blockquote glader cut object param embed'.split()
     else:
         valid_tags = []
+
     valid_attrs = 'href src pic user page class text title alt'.split()
     # параметры видеороликов
     valid_attrs += 'width height classid codebase id name value flashvars allowfullscreen allowscriptaccess quality src type bgcolor base seamlesstabbing swLiveConnect pluginspage data frameborder'.split()
 
     soup = BeautifulSoup(value)
-    for comment in soup.findAll(
-        text=lambda text: isinstance(text, HtmlComment)):
-        comment.extract()
-    for tag in soup.findAll(True):
-        if tag.name not in valid_tags:
-            tag.hidden = True
-        tag.attrs = [(attr, val) for attr, val in tag.attrs
-                     if attr in valid_attrs]
+    for tag in soup.recursiveChildGenerator():
+        if isinstance(tag, element.Tag):
+            if tag.name in valid_tags:
+                tag.attrs = [(attr, val) for attr, val in tag.attrs
+                             if attr in valid_attrs]
+            else:
+                tag.hidden = True
+
+        elif isinstance(tag, element.Comment):
+            tag.extract()
+
     result = soup.renderContents().decode('utf8')
     return re.sub('</(glader|cut)>', '', result)
 
---
+++ src/core/management/commands/check_images.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from itertools import chain
+from bs4 import BeautifulSoup, Tag, Comment
+
+from django.core.management.base import NoArgsCommand
+
+from core.models import Photo, Man, Movie, Mountain, MountainPhoto, Post
+from core.utils.thumbnails import make_thumbnail
+
+def sanitizeHTML(value, mode='none'):
+    """ Удаляет из value html-теги.
+        Если mode==none - все теги
+        Если mode==strict - все теги кроме разрешенных
+    """
+    if mode == 'strict':
+        valid_tags = 'p i strong b u a h1 h2 h3 h4 pre br div span img blockquote glader cut object param embed'.split()
+    else:
+        valid_tags = []
+
+    valid_attrs = 'href src pic user page class text title alt'.split()
+    # параметры видеороликов
+    valid_attrs += 'width height classid codebase id name value flashvars allowfullscreen allowscriptaccess quality src type bgcolor base seamlesstabbing swLiveConnect pluginspage data frameborder'.split()
+    print "SANITIZE"
+    print value
+
+    soup = BeautifulSoup(value)
+    #for comment in soup.findAll(
+    #    text=lambda text: isinstance(text, HtmlComment)):
+    #    comment.extract()
+
+    for tag in soup.recursiveChildGenerator():
+        if isinstance(tag, Tag) and tag.name in valid_tags:
+            tag.attrs = [(attr, val) for attr, val in tag.attrs
+                         if attr in valid_attrs]
+        elif isinstance(tag, Comment):
+            tag.extract()
+        else:
+            tag.hidden = True
+
+    print soup
+    result = soup.renderContents().decode('utf8')
+    return result
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+
+
+        print sanitizeHTML('<asd>asd asd</asd> middle <b>bbb</b> <!-- comment -->', mode='strict')
+        return
+
+        for post in Post.objects.all():
+            try:
+                tree = BeautifulSoup(post.content, "html5lib")
+
+                for tag in tree.find_all('img'):
+                    image = tag.get('src')
+                    if not 'thumb' in image:
+                        if not image.startswith('http') or 'glader.ru' in image:
+                            print post.id, '\t', tag.get('src')
+            except Exception, e:
+                print e
