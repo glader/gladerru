c8b2b34d987e7869db289b625de7134ceb777df8
Mikhail Polykovskij <glader@yandex-team.ru>
2014-05-04 23:44:45 +0600

    Remove uniused app

--- src/settings.py
+++ src/settings.py
@@ -74,7 +74,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'timelog',
     'core',
-    'shop',
     'sape',
     'south',
     'django_messages',
--- src/shop/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from shop.models import Category, Brand, Item
-
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Item)
