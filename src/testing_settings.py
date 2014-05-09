daa0dc3bcf45ce98cced20a9ea80b20a274ea83c
Mikhail Polykovskij <glader@yandex-team.ru>
2014-05-09 14:08:03 +0600

    Travis

---
+++ src/testing_settings.py
@@ -0,0 +1,39 @@
+# development
+
+import logging
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = 'log/email'
+
+SECRET_KEY = '12345'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'upload')
+
+try:
+    os.makedirs(EMAIL_FILE_PATH)
+except Exception:
+    pass
+
+LOG_PATH = 'log'
+LOG_LEVEL = logging.DEBUG
+
+for name, handler in LOGGING['handlers'].items():
+    if 'filename' in handler:
+        handler['filename'] = os.path.join(LOG_PATH, os.path.basename(handler['filename']))
+
+south_logger = logging.getLogger('south')
+south_logger.setLevel(logging.INFO)
+
+INSTALLED_APPS += (
+    'django_nose',
+)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
