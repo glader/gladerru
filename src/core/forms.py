c1c739216254dbef18cf50a32a0414b0e9cefa7b
Mikhail Polykovskij <glader@yandex-team.ru>
2014-09-21 13:09:47 +0600

    Allow tables

--- src/core/forms.py
+++ src/core/forms.py
@@ -19,11 +19,12 @@ def sanitizeHTML(value, mode='none'):
         Если mode==strict - все теги кроме разрешенных
     """
     if mode == 'strict':
-        valid_tags = 'p i strong b u a h1 h2 h3 h4 pre br div span img blockquote object param embed iframe'.split()
+        valid_tags = 'p i strong b u a h1 h2 h3 h4 pre br div span img blockquote object param embed iframe ' \
+                     'table thead tbody tr td'.split()
     else:
         valid_tags = []
 
-    valid_attrs = 'href src pic user page class text title alt style'.split()
+    valid_attrs = 'href src pic user page class text title alt style colspan rowspan'.split()
     # параметры видеороликов
     valid_attrs += 'width height classid codebase id name value flashvars webkitallowfullscreen mozallowfullscreen ' \
                    'allowfullscreen allowscriptaccess ' \
