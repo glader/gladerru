c2eca5c765998d43021341cdc15c93ade8ac31a1
Mikhail Polykovskij <glader@yandex-team.ru>
2014-12-05 00:18:03 +0500

    Simplify movies list

--- src/core/sitemap.py
+++ src/core/sitemap.py
@@ -5,6 +5,6 @@ from .models import Post, Photo
 
 
 sitemaps = {
-    'post': GenericSitemap({"queryset": Post.objects.all()}, changefreq="daily"),
-    'photo': GenericSitemap({"queryset": Photo.objects.all()}, changefreq="daily"),
+    'post': GenericSitemap({"queryset": Post.objects.all()}, changefreq="weekly"),
+    'photo': GenericSitemap({"queryset": Photo.objects.all()}, changefreq="weekly"),
 }
