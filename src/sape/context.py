e61f17c1b0ac24439e403c8df166502635f44ba8
Mikhail Polykovskij <glader@yandex-team.ru>
2015-01-12 13:24:42 +0500

    fix sape fetch

--- src/sape/context.py
+++ src/sape/context.py
@@ -5,5 +5,5 @@ from sape.models import Link
 
 
 def links(request):
-    page_links = Link.objects.filter(status='OK', page=request.META['PATH_INFO'])
+    page_links = Link.objects.filter(status__in=('OK', 'ERROR'), page=request.META['PATH_INFO'])
     return {'sape_links': mark_safe(u', '.join(map(unicode, page_links)))}
