ed2a1ddbf936acb2da56426160bc2e48d67d465f
Mikhail Polykovskij <glader@yandex-team.ru>
2015-11-13 19:38:10 +0500

    Command to clean posts

---
+++ src/core/management/commands/clean_posts.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import bs4
+import re
+
+from django.core.management.base import NoArgsCommand
+
+from core.models import Post
+
+
+def drop_empty_tag(tag):
+    empty = True
+    for child in tag.contents:
+        if isinstance(child, bs4.element.NavigableString):
+            if len(unicode(child).strip()) > 0:
+                empty = False
+                break
+        else:
+            empty = False
+            break
+
+    if empty:
+        try:
+            tag.extract()
+        except Exception:
+            tag.hidden = True
+
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+        for post in Post.objects.all():
+            if not post.content:
+                continue
+
+            print post.id
+            tree = bs4.BeautifulSoup(post.content.encode('utf8'), from_encoding='utf8')
+
+            for tag in tree.find_all('br'):
+                tag.extract()
+
+            for tag in tree.find_all('strong'):
+                drop_empty_tag(tag)
+
+            for tag in tree.find_all('a'):
+                drop_empty_tag(tag)
+
+            for tag in tree.find_all('p'):
+                drop_empty_tag(tag)
+
+            html = tree.renderContents(prettyPrint=True, indentLevel=2)
+            post.content = html
+            post.save()
+
+            post = Post.objects.get(pk=post.pk)
+            post.content = re.sub('</?(html|body)>', '', post.content, re.U)
+            post.save()
