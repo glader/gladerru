286d977c87b365573ccd77c9fc520b2d38c4e9e5
Mikhail Polykovskij <glader.ru@gmail.com>
2018-09-09 19:11:18 +0300

    Бекап с ротацией

--- tools/dump/dump.sh
+++ tools/dump/dump.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-DIR="`date +%%Y_%%m`"
 FILE="`date +%%Y%%m%%d_%%H%%M`"
-FULL_DIR="/backup/%(DUMP_USER)s/gladerru/$DIR"
 ARCHIVE="/tmp/mysqldump_$FILE.sql.gz"
+CURRENT_DIR="./gladerru/current"
 
 echo "Dumps $FILE"
 mysqldump -u%(DATABASE_USER)s -p%(DATABASE_PASSWORD)s %(DATABASE_DB)s | gzip > $ARCHIVE
 
-echo "make dir $DIR"
-ssh %(DUMP_USER)s@%(DUMP_HOST)s "mkdir -p $FULL_DIR"
+echo "make dir"
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "mkdir -p ./gladerru/current"
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "mkdir -p ./gladerru/intermediate"
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "mkdir -p ./gladerru/old"
 
 echo "upload $FILE"
-scp $ARCHIVE %(DUMP_USER)s@%(DUMP_HOST)s:$FULL_DIR
+scp $ARCHIVE %(DUMP_USER)s@%(DUMP_HOST)s:$CURRENT_DIR
+
+echo "move old files"
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "find ./gladerru/current -mtime +3 | grep _0000 | xargs -i mv {} ./gladerru/intermediate"
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "find ./gladerru/current -mtime +3 -delete"
+
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "find ./gladerru/intermediate -mtime +10 | grep 01_0000 | xargs -i mv {} ./gladerru/old"
+ssh %(DUMP_USER)s@%(DUMP_HOST)s "find ./gladerru/intermediate -mtime +10 -delete"
+
